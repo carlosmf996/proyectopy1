d7722e86e668d3706cd37cc2b35c0e290a3b544a
carlosmf996 <carlosmf996@gmail.com>
2023-10-10 09:40:20 +0200

    Depuración y busqueda por input

--- main.py
+++ main.py
@@ -8,36 +8,34 @@ import requests
 url = 'https://api.sampleapis.com/beers/ale'
 
 try:
-    # Realizar la solicitud GET a la API
+    # Guardamos la URL en una variable
     response = requests.get(url)
 
-    # Comprobar si la solicitud fue exitosa (código de estado 200)
+    # Comprobamos si la URL está operativa, si no, devuelve un error
     if response.status_code == 200:
-        # Analizar la respuesta JSON
         data = response.json()
 
-        # Ahora 'data' contiene el JSON cargado
-        #print(data)
+    # Aquí se muestra el error que devuelve la API en caso de fallar
     else:
         print('Error al hacer la solicitud a la API:', response.status_code)
 
 except requests.exceptions.RequestException as e:
-    print('Error de conexión:', e)
+    print('Error de conexión:', e)          # Envolvemos todo en un TRY - EXCEPT para contemplar errores
 
 
 #--------------------------MOSTRAMOS DATOS--------------------------
 
 
-nombre_buscar = "Founders All Day IPA"  # Reemplaza con el nombre que deseas buscar
-objeto_deseado = None
+nombreCerveza = str(input("Busca cerveza por su nombre"))
+cervezaEncontrada = " "
 
-for elemento in data:
-    if elemento.get("name") == nombre_buscar:
-        objeto_deseado = elemento
+for cerveza in data:
+    if cerveza.get("name") == nombreCerveza:
+        cervezaEncontrada = cerveza
         break
 
-if objeto_deseado:
+if cervezaEncontrada:
     # Ahora 'objeto_deseado' contiene el objeto con el nombre buscado
-    print(objeto_deseado)
+    print(cervezaEncontrada)
 else:
-    print(f"No se encontró ningún objeto con el nombre '{nombre_buscar}'")
+    print(f"No se encontró ningún objeto con el nombre '{nombreCerveza}'")
