92220a9c0870e1a3ae0350bf688a428929eff579
carlosmf996 <carlosmf996@gmail.com>
2023-10-10 09:28:53 +0200

    Comienzo a buscar datos en el JSON

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import requests
 
+
+#--------------------------CARGAMOS EL JSON--------------------------
+
+
 # URL de la API que contiene el archivo JSON
 url = 'https://api.sampleapis.com/beers/ale'
 
@@ -13,9 +17,27 @@ try:
         data = response.json()
 
         # Ahora 'data' contiene el JSON cargado
-        print(data)
+        #print(data)
     else:
         print('Error al hacer la solicitud a la API:', response.status_code)
 
 except requests.exceptions.RequestException as e:
     print('Error de conexión:', e)
+
+
+#--------------------------MOSTRAMOS DATOS--------------------------
+
+
+nombre_buscar = "Founders All Day IPA"  # Reemplaza con el nombre que deseas buscar
+objeto_deseado = None
+
+for elemento in data:
+    if elemento.get("name") == nombre_buscar:
+        objeto_deseado = elemento
+        break
+
+if objeto_deseado:
+    # Ahora 'objeto_deseado' contiene el objeto con el nombre buscado
+    print(objeto_deseado)
+else:
+    print(f"No se encontró ningún objeto con el nombre '{nombre_buscar}'")
