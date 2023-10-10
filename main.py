62f3ab23aee4a68373d76ca6b3ad6a564a8d7bda
carlosmf996 <carlosmf996@gmail.com>
2023-10-10 11:27:02 +0200

    Creo el menú y funciones

--- main.py
+++ main.py
@@ -23,31 +23,72 @@ except requests.exceptions.RequestException as e:
     print('Error de conexión:', e)          # Envolvemos todo en un TRY - EXCEPT para contemplar errores
 
 
-#--------------------------MOSTRAMOS DATOS--------------------------
+
+
+#--------------------------FUNCIONES--------------------------
+
+def buscarNombre():     #Esta función permite la búsqueda de cervezas por nombre
 
 # Pedimos los datos al usuario por consola y los mostramos
-nombreCerveza = str(input("Busca cerveza por su nombre: ")).upper()
-cervezaEncontrada = ""
-encontrada = False
-indice=0
+    nombreCerveza = str(input("Busca cerveza por su nombre: ")).upper()
+    cervezaEncontrada = ""
+    encontrada = False
+    indice=0
 
-# Hacer búsquedas con un FOR es bastante poco eficaz por lo que busco con un WHILE
-while indice<len(data) and encontrada == False:
-    if data[indice].get("name").upper() == nombreCerveza:
-        cervezaEncontrada = data[indice]
-        encontrada=True
+    # Hacer búsquedas con un FOR es bastante poco eficaz por lo que busco con un WHILE
+    while indice<len(data) and encontrada == False:
+        if data[indice].get("name").upper() == nombreCerveza:
+            cervezaEncontrada = data[indice]
+            encontrada=True
+        else:
+            indice+=1
+
+    # Doy formato a la información mostrada
+    if cervezaEncontrada != "":
+        print("")
+        print("NOMBRE: ",cervezaEncontrada.get("name"))
+        print("PRECIO: ",cervezaEncontrada.get("price"))
+        print("NOTA:")
+        print("   Nº RESEÑAS",cervezaEncontrada.get("rating").get("reviews"))
+        media = cervezaEncontrada.get("rating").get("average")
+        mostrarMedia = f"{media:.2f}"
+        print("   NOTA", mostrarMedia)
+
     else:
-        indice+=1
-
-if cervezaEncontrada != "":
-    print("NOMBRE: ",cervezaEncontrada.get("name"))
-    print("PRECIO: ",cervezaEncontrada.get("price"))
-    print("NOTA:")
-    print("   Nº RESEÑAS",cervezaEncontrada.get("rating").get("reviews"))
-    media = cervezaEncontrada.get("rating").get("average")
-    mostrarMedia = f"{media:.2f}"
-    print("   NOTA", mostrarMedia)
+        print("No se encontró ningún objeto con el nombre ", nombreCerveza)
 
 
-else:
-    print("No se encontró ningún objeto con el nombre ", nombreCerveza)
+
+
+def mostrarMenu():          #Esta función muestra el menú por el que navegaremos
+
+    print("")
+    print("----------CERVECERIA LQTC----------")
+    print("")
+    print("1.- Búsqueda por nombre")
+    print("2.- Búsqueda por precio")
+    print("3.- Búsqueda por nota media")
+    print("4.- Cervezas ordenadas por precio")
+    print("5.- Cervezas ordenadas por nota media")
+    print("6.- SALIR")
+    print("")
+
+    numeroMenu = int(input("Introduzca opción del menú: "))
+
+    if numeroMenu > 6 or numeroMenu < 1:            #Controlamos que el número introducido sea correcto
+        print("")
+        print("INTRODUZCA UN NÚMERO CORRECTO")
+        print("")
+        mostrarMenu()
+
+    if numeroMenu == 1:
+        buscarNombre()
+
+    if numeroMenu == 6:
+        print("¡¡¡Vuelve pronto!!!")
+        exit        
+
+
+#--------------------------EJECUTAMOS EL PROGRAMA--------------------------
+
+mostrarMenu()
